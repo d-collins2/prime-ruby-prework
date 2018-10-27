74ef54e87aea97f1964994c398979ac10d891965
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-27 11:00:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,7 @@
 # Add  code here!
 def prime?(num)
   return false if num <= 0 
-  arr = []
-  (2..num).each{ |n| arr << n if num % n == 0 }
- 
-  if arr.size == 2 
-    return false  
-  else 
-    return true 
-  end 
+  
+  (2...num).each{ |n|  return false num % n == 0 }
+  return true 
 end 
